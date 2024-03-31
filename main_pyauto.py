adef82e8bd410852d0ed3b0f31307c3a98419898
savkorlev <evglev997@gmail.com>
2024-03-31 22:41:12 +0200

    Fixed a bug

--- main_pyauto.py
+++ main_pyauto.py
@@ -6,6 +6,8 @@ from email.mime.multipart import MIMEMultipart
 import cv2
 import numpy
 import json
+import time
+import pygame
 
 # Load the input data
 with open('resources/input_data.json', 'r') as file:
@@ -20,28 +22,48 @@ screenshot_coordinates = input_data['screenshot_coordinates']
 next_coordinates = input_data['next_coordinates']
 close_coordinates = input_data['close_coordinates']
 
-# Setting up the MIME
-message = MIMEMultipart()
-message['From'] = sender_email
-message['To'] = receiver_email
-message['Subject'] = 'Appointment found!'
-body = 'Appointment found!'
-message.attach(MIMEText(body, 'plain'))
+def send_email():
+    # Setting up the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = 'Appointment found!'
+    body = 'Appointment found!'
+    message.attach(MIMEText(body, 'plain'))
+    # Create SMTP session for sending the email
+    session = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    session.starttls() # enable security
+    session.login(sender_email, sender_password)
+    session.sendmail(sender_email, receiver_email, message.as_string())
+    print('Mail Sent')
+    session.quit()
+
+def play_music(file_path, duration_in_seconds):
+    # Initialize pygame mixer
+    pygame.mixer.init()
+    # Load the music file
+    pygame.mixer.music.load(file_path)
+    # Start playing the music
+    pygame.mixer.music.play()
+    # Play music for the specified duration
+    time.sleep(duration_in_seconds)
+    # Stop the music
+    pygame.mixer.music.stop()
 
 chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
 subprocess.Popen([chrome_path, target_url])
-pyautogui.sleep(5)
+time.sleep(5)
 
 pyautogui.click(dropdown_coordinates[0], dropdown_coordinates[1])
 pyautogui.press('down')
 pyautogui.press('enter')
-pyautogui.sleep(30)
+time.sleep(30)
 
 pyautogui.click(verification_coordinates[0], verification_coordinates[1])
 
 for i in range(1,6):  # 5 months
-    pyautogui.sleep(1)
+    time.sleep(1)
 
     # Step 1: Take a screenshot of a specific area and save it
     screenshot_region = (screenshot_coordinates[0], screenshot_coordinates[1], 595, 128)
@@ -61,15 +83,11 @@ for i in range(1,6):  # 5 months
 
     if numpy.any(difference):
         print('Appointment found!')
-        # Create SMTP session for sending the mail
-        session = smtplib.SMTP('smtp-mail.outlook.com', 587)
-        session.starttls() # enable security
-        session.login(sender_email, sender_password)
-        session.sendmail(sender_email, receiver_email, message.as_string())
-        print('Mail Sent')
-        session.quit()
+        send_email()
+        play_music('resources/Anthem of Europe.mp3', 60)
     else:
         print('No free appointments')
-        pyautogui.click(next_coordinates[0], next_coordinates[1])
+
+    pyautogui.click(next_coordinates[0], next_coordinates[1])
 
 pyautogui.click(close_coordinates[0], close_coordinates[1])
